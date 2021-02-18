2c982da0d413f80a8a1e528b5ee8f2c291c167fc
unknown <pcolantuomo@gmail.com>
2021-02-18 01:12:45 -0300

    chore: adding head

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import "../styles/globals.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <>
-    <Head>
-      <title>Trending5</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>Trending5</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
