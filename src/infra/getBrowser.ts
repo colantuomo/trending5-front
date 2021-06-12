d561a4f72c8acd1a989ad27d06ae7ca0a48a7c78
Yuri Almeida <yurialves2@gmail.com>
2021-06-12 14:04:54 -0300

    using playwright

--- src/infra/getBrowser.ts
+++ src/infra/getBrowser.ts
@@ -1,35 +1,10 @@
-import chrome from "chrome-aws-lambda";
-import puppeteer, { Page, Browser } from "puppeteer";
-
-const chromeExecPaths = {
-  win32: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-  linux: "/usr/bin/google-chrome",
-  darwin: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-};
-
-async function getOptions() {
-  const isDev = !process.env.AWS_REGION;
-  const exePath = chromeExecPaths[process.platform];
-  return isDev
-    ? {
-        args: [],
-        headless: true,
-        executablePath: exePath,
-      }
-    : {
-        args: chrome.args,
-        headless: chrome.headless,
-        executablePath: await chrome.executablePath,
-      };
-}
+import playwright, { Browser, Page } from "playwright";
 
 let browser: Browser | undefined;
 
 export async function getBrowser() {
-  // const options = await getOptions();
-
   if (!browser) {
-    browser = await puppeteer.launch({ headless: true });
+    browser = await playwright.chromium.launch({ headless: true });
   }
   return browser;
 }
